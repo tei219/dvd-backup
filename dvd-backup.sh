da1ffcad012a5fe8f95737695d6d8f0bfaaf5dcf
tei219 <tei219@gmail.com>
2020-02-22 15:04:11 +0900

    fix

--- dvd-backup.sh
+++ dvd-backup.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 LANG=C
-cd /iso
+cd /root/dvd-backup
 
-LOG=backup.log
+DEVN="$1"
+DEVNAME="/dev/$1"
+MOUNTDIR="/media/cdrom/$1"
+LOG="./backup.$1.log"
+CREATEDATE="createdate.$1"
+UDEVINFO="udevinfo.$1"
+
+mkdir -p ${MOUNTDIR} 2>/dev/null 
 
 function _log() {
-  echo "$(date) $*" >> ${LOG} 
+  echo "$(date) ${DEVN} $*" >> ${LOG} 
 }
 
-_log "udev"
-if [[ ! -z $(udevadm info -p $(udevadm info -q path /dev/sr0) | grep ID_FS) ]]; then
+_log "udev $1"
+if [[ ! -z $1 && ! -z $(udevadm info -p $(udevadm info -q path ${DEVNAME}) | grep ID_FS) ]]; then
   _log "detected"
 
   _log "check udevinfo"
-  udevadm info -p $(udevadm info -q path /dev/sr0) > udevinfo
+  udevadm info -p $(udevadm info -q path ${DEVNAME}) > ${UDEVINFO}
   _log "check creation date"
-  mount -t auto /dev/sr0 /media && stat -c "%x" /media > createdate && umount /media 
+  mount -t auto ${DEVNAME} ${MOUNTDIR} && stat -c "%x" ${MOUNTDIR} > ${CREATEDATE} && umount ${MOUNTDIR}
 
-  CREATE_DATE=$(cat createdate | sed -e 's/[+ ]/_/g' -e 's/:/-/g')
-  DVD_LABEL=$(cat udevinfo | grep ID_FS_LABEL= | awk -F= '{print $2}')
+  CREATE_DATE=$(cat ${CREATEDATE} | sed -e 's/[+ ]/_/g' -e 's/:/-/g')
+  DVD_LABEL=$(cat ${UDEVINFO} | grep ID_FS_LABEL= | awk -F= '{print $2}')
   FN_ISO=${DVD_LABEL}_${CREATE_DATE}.iso
-  _log "check info (handbrake)"
-  HandBrakeCLI -i /dev/sr0 -t 0 > ${FN_ISO}.handinfo 2>&1
-  _log "check info (dvdbackup)"
-  dvdbackup -i /dev/sr0 -I > ${FN_ISO}.dvdinfo 2>&1
+  if [[ ! -e ${FN_ISO} ]]; then
+    _log "check info (handbrake)"
+    HandBrakeCLI -i ${DEVNAME} -t 0 > ${FN_ISO}.handinfo 2>&1
+    _log "check info (dvdbackup)"
+    dvdbackup -i ${DEVNAME} -I > ${FN_ISO}.dvdinfo 2>&1
 
-  _log "making iso file ... ${FN_ISO}"
-  dd if=/dev/sr0 of=${FN_ISO}
+    _log "making iso file ... ${FN_ISO}"
+    dd if=${DEVNAME} of=${FN_ISO}
+#    dd if=${DEVNAME} | tee ${FN_ISO} | sha256sum > ${FN_ISO}.${DEVN}.sum 
 
-  _log "verifiy image sr0"
-  sha256sum /dev/sr0 > ${FN_ISO}.sr0.sum
+    _log "verify image ${DEVNAME}"
+   sha256sum ${DEVNAME} > ${FN_ISO}.${DEVN}.sum
 
-  _log "verifiy image backup"
-  sha256sum ${FN_ISO} > ${FN_ISO}.sum
-  sleep 3
-
+    _log "verify image backup"
+    sha256sum ${FN_ISO} > ${FN_ISO}.sum
+    sleep 3
+  else
+    _log "already ripped"
+  fi
   _log "ejected" 
-  eject
+  eject ${DEVNAME}
   sleep 3
   _log "script end"
 fi
